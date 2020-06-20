68123c427e86fccfcfa9e22d48a437f0a83bef58
arunas8115@gmail.com <arunas8115@gmail.com>
2020-06-20 10:44:57 +0530

    authentication added

--- cdmapp/views.py
+++ cdmapp/views.py
@@ -10,85 +10,102 @@ from rest_framework.authentication import SessionAuthentication,TokenAuthenticat
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from cdmapp import models
 from cdmapp import serializers
- 
+
 class NationViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.NationSerializer
   queryset = {}
   queryset = models.Nation.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class StateViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.StateSerializer
   queryset = {}
   queryset = models.State.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class DistrictViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.DistrictSerializer
   queryset = {}
   queryset = models.District.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class PanchayatViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.PanchayatSerializer
   queryset = {}
   queryset = models.Panchayat.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class WardViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.WardSerializer
   queryset = {}
   queryset = models.Ward.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class GenderViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.GenderSerializer
   queryset = {}
   queryset = models.Gender.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class CustomerViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.CustomerSerializer
   queryset = {}
   queryset = models.Customer.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class CustomerDocumentViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.CustomerDocumentSerializer
   queryset = {}
   queryset = models.Customer_document.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class CheckinMasterViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.CheckinMasterSerializer
   queryset = {}
   queryset = models.Checkin.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class CheckoutMasterViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.CheckoutMasterSerializer
   queryset = {}
   queryset = models.Checkout.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class CompanyMasterViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.CompanyMasterSerializer
   queryset = {}
   queryset = models.Company_master.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class PricingPlanViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.PricingPlanSerializer
   queryset = {}
   queryset = models.Pricing_plan.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class SettingsViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.SettingsSerializer
   queryset = {}
   queryset = models.Settings.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class UserRoleViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.UserRoleSerializer
   queryset = {}
   queryset = models.User_role.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class RolePermissionViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.RolePermissionSerializer
   queryset = {}
   queryset = models.Role_permission.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
 
 class UserViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.UserSerializer
   queryset = {}
   queryset = models.User.objects.filter().order_by(('id'))
+  permission_classes = [IsAuthenticated,]
